d9a705e448303298ab029fc8478e2b99aa5a1c32
Shakesz <shakeszka@nyu.edu>
2026-06-11 16:35:06 +0500

    added a new tour

--- app-files/data.js
+++ app-files/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0--",
-      "name": "Входная часть",
+      "id": "0-",
+      "name": "Вход",
       "levels": [
         {
           "tileSize": 256,
@@ -22,25 +22,31 @@ var APP_DATA = {
           "size": 2048
         }
       ],
-      "faceSize": 1500,
+      "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 0.6733874163783113,
-        "pitch": 0.0261312628559196,
-        "fov": 1.5779262570687063
+        "yaw": -0.1044787058334613,
+        "pitch": 0.004002028858080209,
+        "fov": 1.5707963267948966
       },
       "linkHotspots": [
         {
-          "yaw": 1.2318111010953317,
-          "pitch": 0.07437798762680714,
+          "yaw": 0.6011896656444513,
+          "pitch": 0.039424825775192573,
           "rotation": 0,
           "target": "1-"
+        },
+        {
+          "yaw": -0.46124701113192934,
+          "pitch": 0.03431520807482791,
+          "rotation": 0,
+          "target": "2--"
         }
       ],
       "infoHotspots": []
     },
     {
       "id": "1-",
-      "name": "Балкон",
+      "name": "Летка",
       "levels": [
         {
           "tileSize": 256,
@@ -60,24 +66,280 @@ var APP_DATA = {
           "size": 2048
         }
       ],
-      "faceSize": 1500,
+      "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 0.7632906595388498,
-        "pitch": 0.0544284929119776,
-        "fov": 1.5779262570687063
+        "yaw": -0.39239738791297185,
+        "pitch": 0.12323539522979843,
+        "fov": 1.5707963267948966
       },
       "linkHotspots": [
         {
-          "yaw": 2.8388465714787,
-          "pitch": 0.08061663987734846,
+          "yaw": 1.6277027242383966,
+          "pitch": 0.06843317271482618,
           "rotation": 0,
-          "target": "0--"
+          "target": "0-"
+        },
+        {
+          "yaw": -1.7296534944210258,
+          "pitch": 0.12178025116854485,
+          "rotation": 0,
+          "target": "3--"
+        },
+        {
+          "yaw": -2.905633744696324,
+          "pitch": 0.18153919833614296,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2--",
+      "name": "Первый зал",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.8919125643756551,
+        "pitch": 0.13988596443108214,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1311415907668465,
+          "pitch": 0.044625691056804584,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 2.6678664359110034,
+          "pitch": 0.08424927438647956,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--",
+      "name": "Второй зал",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.3170523086818635,
+        "pitch": 0.03250482954370071,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.12591130310041,
+          "pitch": 0.13315660348972003,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -2.407228110325274,
+          "pitch": 0.14778585617549922,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -2.285277828806194,
+          "pitch": 0.629877755744344,
+          "rotation": 0,
+          "target": "6---2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "Касса",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.1561980276577426,
+        "pitch": 0.03502203150268812,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5581032467781206,
+          "pitch": 0.18598925754865547,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 1.7198824396391341,
+          "pitch": 0.12341554812752165,
+          "rotation": 0,
+          "target": "3--"
+        },
+        {
+          "yaw": -0.8622243646554661,
+          "pitch": 0.1031175674209095,
+          "rotation": 0,
+          "target": "2--"
+        },
+        {
+          "yaw": -2.514328809384896,
+          "pitch": 0.10166964960225755,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 1.5117964463116271,
+          "pitch": 0.3022198154101332,
+          "rotation": 0,
+          "target": "5--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5--",
+      "name": "Нижний зал",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.1562187392047498,
+        "pitch": 0.36555812143282296,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3585105818136896,
+          "pitch": 0.13022648133839887,
+          "rotation": 0,
+          "target": "6---2"
+        },
+        {
+          "yaw": -0.855537163383147,
+          "pitch": -0.4480138760822747,
+          "rotation": 0,
+          "target": "3--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6---2",
+      "name": "Нижний зал 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.8195022914639907,
+        "pitch": 0.15302095236951097,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2719347616182084,
+          "pitch": 0.18497737815048332,
+          "rotation": 0,
+          "target": "5--"
+        },
+        {
+          "yaw": -0.5126231175117404,
+          "pitch": -0.6575547931458523,
+          "rotation": 0,
+          "target": "3--"
         }
       ],
       "infoHotspots": []
     }
   ],
-  "name": "Vira 3D - Демо",
+  "name": "Ресторан Alcha",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
